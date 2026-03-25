a81f6021733b499945f9f2bef146a4ff02f9681e
Jean-Baptiste Guérin <Jean-Baptiste.Guerin@uphf.fr>
2026-03-25 15:30:07 +0100

    sauvegarde avant suppression temporaire bdd

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('upload/', views.upload_image, name='upload_image'),
     path('', views.home, name='home'),
     path('form/', views.form_view, name='form'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Utilisateur
+import google.generativeai as genai
+from PIL import Image
+import os
+from django.conf import settings
+
+# Configuration Gemini
+genai.configure(api_key="TA_CLE_API")
+
 
 def home(request):
     utilisateurs = Utilisateur.objects.all()
@@ -16,3 +24,31 @@ def form_view(request):
         return redirect('home')
 
     return render(request, 'form.html')
+
+
+def upload_image(request):
+    result = ""
+
+    if request.method == "POST" and request.FILES.get("image"):
+        file = request.FILES["image"]
+
+        media_path = os.path.join(settings.MEDIA_ROOT, file.name)
+
+        os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
+
+        with open(media_path, "wb+") as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        image = Image.open(media_path)
+
+        model = genai.GenerativeModel('gemini-1.5-flash')
+
+        response = model.generate_content([
+            "Je souhaite évaluer le plan suivant, représentant un outillage pour fonderie. Ce qui m'intéresse, c'est les dimensions totales de l'outillage. Saurais tu procéder à un OCR, et de reconstituer les côtes principales sur ce plan, et d'estimer le poids ?",
+            image
+        ])
+
+        result = response.text
+
+    return render(request, "upload.html", {"result": result})
